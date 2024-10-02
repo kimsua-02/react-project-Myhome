f7470b598a4fffde964de65b9548c4e94def04db
RRRR <msgpigeon@gmail.com>
2024-10-02 17:21:26 +0900

    AAAAAA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import IdlePage from "./IdlePage";
+
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/idle" element ={<IdlePage/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/Result.js
+++ src/Result.js
@@ -3,3 +3,8 @@
  * PurchaseMenu.js에서 클릭한 정보를 받아 실제로 결제하는 것처럼 보이게 구현하기
  * 영수증, 주문번호 출력 후 IdlePage로 이동
  */
+
+const Result = ({ PurchaseMenu }) =>
+{
+    
+}
