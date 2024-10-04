c2227afd700abf958068cd94c59c0240260eaf28
YSJ <dbstjrwnd123@gmail.com>
2024-10-04 11:24:14 +0900

    상세메뉴장바구니

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import IdlePage from "./IdlePage";
+import ExtraShot from "./ExtraShot";
+import ShoppingCart from "./ShoppingCart";
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
+  const addCart = (menuItem) => {
+    setCart((prevMenu) => [...prevMenu, menuItem])
+  }
+
   return (
     <>
     <BrowserRouter>
       <Routes>
         <Route path = "/idle" element ={<IdlePage/>}/>
+        <Route path ="menu">
+          <Route index element={<MainMenu/>}/>
+        <Route path =":menucode" element={<ExtraShot addCart={addCart}/>}/>
+        </Route>
+      <Route path="shoppingcart" element={<ShoppingCart cart={cart}/>}/>
       </Routes>
     </BrowserRouter>
     </>
