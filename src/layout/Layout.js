04201725c76b4cd9038f7c973fb2557092cb5eaa
YSJ <dbstjrwnd123@gmail.com>
2024-10-07 12:28:18 +0900

    상세메뉴/쇼핑카트

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,14 +6,8 @@ import Coffee from "../main-menu-page/drink/Coffee";
 
 
 const Layout = ()=>{
-
     return(
-<<<<<<< HEAD
-        <>
-=======
-
         <>  
->>>>>>> d05a3388389bee00979abee3d4ae7c1d7c515d5e
             <Nav/>
             <Outlet/>
         </>
