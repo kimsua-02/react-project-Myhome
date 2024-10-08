c2b64435da098142dfe8f87fd3609bbcfef5e492
YSJ <dbstjrwnd123@gmail.com>
2024-10-08 15:05:23 +0900

    YSJ

--- src/MenuDetail.js
+++ src/MenuDetail.js
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import {useState, useEffect, useReducer } from "react";
+import {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import { drinkgetMenuDetail,dessertgetMenuDetail,mdgetMenuDetail } from "./main-menu-page/MenuAPI";
 import { ExtraIce, ExtraShot, ExtraSugar, ExtraTopping } from "./option/Option";
